d36fce6606a5462efc9aaff764bc46b2a3bcd5bf
JosueNavarroSolis <56096921+JosueNavarroSolis@users.noreply.github.com>
2020-11-13 00:02:19 -0600

    Action

--- Project-1/sources/XML_Painter.cpp
+++ Project-1/sources/XML_Painter.cpp
@@ -3,7 +3,7 @@
 XML_Painter::XML_Painter(int pColortouse) {
     color_to_use = null;
     countries = null;
-    totalcolors={"#009688","#000000","#76448A","#B7950B","#229954","#BA4A00","#979A9A","#283747","#FF00FF","#00FFFF","#00FF00","#FFFF00","#808000"}
+    totalcolors={"#009688","#000000","#76448A","#B7950B","#229954","#BA4A00","#979A9A","#283747","#FF00FF","#00FFFF","#00FF00","#FFFF00","#808000"};
 }
 void XML_Painter::paint_contry() {
     //lo tiene kris
