b97c29c2b572776f9973989c4df2a8ee4bd3e1da
JosueNavarroSolis <56096921+JosueNavarroSolis@users.noreply.github.com>
2020-11-21 01:58:49 -0600

    .

--- Project-1/sources/DynamicAlgorithm.cpp
+++ Project-1/sources/DynamicAlgorithm.cpp
@@ -1,18 +1,26 @@
 #include "../headers/DynamicAlgorithm.h"
 
 vector<int> Dynamic::AlterPriority (vector<int> pPrioritiesValue){
-    int maxindex =0;
-    for (int index = 0;index < pPrioritiesValue.size();index++);
+    int maxindex = 0;
+    int priority = pPrioritiesValue.size();
+    for (int indexround = 0; indexround < pPrioritiesValue.size(); indexround++){
+        for (int index = 0;index < pPrioritiesValue.size();index++){
+            if (pPrioritiesValue[index] > pPrioritiesValue[maxindex]){
+                maxindex = index;
+            }
+        }
+        priorities.at(maxindex) = priority;
+        pPrioritiesValue.at(maxindex) = 0;
+        priority--;
+    }
 }
 
 void Dynamic::DynamicAlgoritm (List pListCountries,int pCounter){
 
     vector<Country> ListToPaint;
     vector<int> counterforcountries;
-    for (int pass = pCounter-30; pass < pCounter;pass++){
-
-
-
+    for (int pass = pCounter; pass < pCounter+10;pass++){
+        
     }
 
 }
