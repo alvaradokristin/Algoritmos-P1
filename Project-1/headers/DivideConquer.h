6cd7c479bd55b3026dca1bccbd2d273d6198c590
Kristin Alvarado <alvaradokristin@gmail.com>
2020-11-20 19:23:20 -0600

    Divide and Conquer implemented.
    Need to fix several errors (multiple definition of...)

--- Project-1/headers/DivideConquer.h
+++ Project-1/headers/DivideConquer.h
@@ -5,19 +5,26 @@
 #ifndef PROJECT_1_DIVIDECONQUER_H
 #define PROJECT_1_DIVIDECONQUER_H
 
-#include "Painter.h"
+#include "../sources/Painter.cpp"
 #include "List.h"
 
 namespace headers {
     class DivideConquer : public Painter {
 
     public:
+        DivideConquer(int pNumberColors);
+        //void Create(int num);
         void divideList(List pDivList);
         void conquerSubG(pointerCntry pInit, pointerCntry pPivot, pointerCntry pBorder, List pList);
         pointerCntry sortSubG(pointerCntry pPivot, pointerCntry pBorder, List pList);
         void paintGroup(pointerCntry pInit, pointerCntry pPivot);
         void doubleCheck(List pList);
 
+    private:
+        vector<string> vectorDC;
+        int countriesToColor;
+        int whiteCountries;
+        int colorsToUse;
     };
 }
 
--- Project-1/sources/DivideConquer.cpp
+++ Project-1/sources/DivideConquer.cpp
@@ -5,8 +5,21 @@
 #include <iostream>
 #include "../headers/DivideConquer.h"
 
-//using namespace headers;
+using namespace headers;
 using namespace std;
+
+DivideConquer::DivideConquer(int pNumberColors) : Painter(pNumberColors) {
+    colorsToUse = pNumberColors;
+    countriesToColor = 211;
+    whiteCountries = 0;
+}
+
+/*void DivideConquer::Create(int num) {
+    write = new XMLPainter();
+    write->numberofcolors = num;
+    write ->SingColors();
+    colors = &write->color_to_use;
+}*/
 
 // This will divide the list of countries in groups of 10
 void DivideConquer::divideList(List pDivList) {
@@ -23,7 +36,9 @@ void DivideConquer::divideList(List pDivList) {
             pivot = border;
         }
     }
-    conquerSubG(init, pivot, border, pDivList);
+    if (init != nullptr) { // In case there are any subgrups left
+        conquerSubG(init, pivot, border, pDivList);
+    }
     doubleCheck(pDivList);
 }
 
@@ -79,7 +94,38 @@ pointerCntry DivideConquer::sortSubG(pointerCntry pPivot, pointerCntry pBorder, 
 
 // This will paint a group of countries, it will ask for each country if they can be X priority
 void DivideConquer::paintGroup(pointerCntry pInit, pointerCntry pPivot) {
+    pointerCntry cntry = pInit;
+    int contriesClrd = 0; // Countries already colored and this will determine the color (priority), if is gratter than the amount of colors to use, then it will paint it White
 
+    if (cntry == pPivot) {
+        while (cntry != pPivot -> nextCntry) {
+            if (!cntry -> isColored) { // If the country is not colored
+                if (cntry != pInit) {
+                    if (contriesClrd < colorsToUse) {
+                        vectorDC = paint_contries(vectorDC, cntry->id, contriesClrd);
+                        cntry->updateColor(colors->at(contriesClrd));
+                        contriesClrd++;
+                    }
+                    else {
+                        vectorDC = paint_contries(vectorDC, cntry->id, 12);
+                        cntry->updateColor("#ffffff");
+                        contriesClrd++;
+                        whiteCountries++;
+                    }
+                }
+                else {
+                    vectorDC = paint_contries(vectorDC, cntry->id, 0);
+                    cntry->updateColor(colors->at(0));
+                    contriesClrd++;
+                }
+            }
+            cntry = cntry -> nextCntry;
+        }
+    }
+    else {
+        vectorDC = paint_contries(vectorDC, cntry->id, 0);
+        cntry->updateColor(colors->at(0));
+    }
 }
 
 // Once all the list is painted, this will double check that there are no adj with the same color, if there are, it will update the color
@@ -88,10 +134,39 @@ void DivideConquer::doubleCheck(List pList) {
     int numCntrs = pList.listLength();
 
     for (int elmts = 0; elmts < numCntrs; elmts++) {
+        vector<string> colorsUsed; // This is a ventor of all the colors used on the adjacents
+        bool isUsed = false; // If the color of this country os already used on the adjacents, isUsed will be true
         for(auto adjs : *countries -> adjVector) {
             if (countries -> color == adjs -> color) {
-                
+                isUsed = true;
+            }
+            colorsUsed.push_back(adjs -> color);
+        }
+        if (isUsed) {
+            int cont = 0;
+            bool isReColored = false;
+            for(string colrs : *colors) {
+                bool isFound = true;
+                for(string colrsUsed : *colors) {
+                    if (colrs == colrsUsed) {
+                        isFound = false;
+                        break;
+                    }
+                }
+                if (isFound) {
+                    vectorDC = paint_contries(vectorDC, countries->id, cont);
+                    countries->updateColor(colors->at(cont));
+                    isReColored = true;
+                    break;
+                }
+                cont++;
+            }
+            if (!isReColored) { // This will paint the country: White since all the color available are used
+                vectorDC = paint_contries(vectorDC, countries->id, 12);
+                countries->updateColor("#ffffff");
+                whiteCountries++;
             }
         }
+        countries = countries -> nextCntry;
     }
 }
